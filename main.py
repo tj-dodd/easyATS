12fcf7b4d42fb099f3f979fe0128beb1d6b00c81
tj-dodd <51852176+tj-dodd@users.noreply.github.com>
2024-06-22 19:58:33 +1000

    Flask integration

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from docx2python import docx2python
-import pdfplumber
-import re
+from flask import Flask, request, render_template, redirect, url_for
 import os
+import re
+import pdfplumber
 import pypandoc
-import comtypes.client
-import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from docx2python import docx2python
+import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = '/uploads'
 
 def extract_text_docx(path):
     with docx2python(path) as docx_content:
@@ -23,20 +26,6 @@ def extract_text_pdf(path):
             text += page.extract_text()
     return text
 
-def convert_doc_docx(path):
-    word = comtypes.client.CreateObject('Word.Application')
-    abs = os.path.abspath(path)
-    doc = word.Documents.Open(abs)
-    docx_path = os.path.splitext(abs)[0] + '.docx'
-    doc.SaveAs(docx_path, FileFormat=16)
-    doc.Close()
-    word.Quit()
-    return docx_path
-
-def extract_text_from_doc(path):
-    docx_path = convert_doc_docx(path)
-    return extract_text_docx(docx_path)
-
 def extract_text_from_rtf(file_path):
     return pypandoc.convert_file(file_path, 'plain')
 
@@ -48,34 +37,11 @@ def screen_resume(resume_text, keywordsString):
     hits = sum(1 for keyword in keywordsString if re.search(r'\b' + re.escape(keyword) + r'\b', resume_text, re.IGNORECASE))
     return hits
 
-keywordFile = open('keywords.txt', 'r', encoding='utf-8')
-keywordsString = keywordFile.read().split(',')
-
-descriptionFile = open('jobdescription.txt', 'r') 
-descriptionString = descriptionFile.read()
-
-cleanDescription = descriptionString.lower()
-cleanDescription = re.sub(r'[^\w\s]', '', cleanDescription)
-cleanDescription = cleanDescription.strip()
-cleanDescription = re.sub('[0-9]+', '', cleanDescription)
-cleanDescription = word_tokenize(cleanDescription)
-stop = stopwords.words('english')
-cleanDescription = [w for w in cleanDescription if not w in stop] 
-cleanDescription = ' '.join(cleanDescription)
-
-resumePaths = []
-resumeStartPath = "resumes/"
-
-resumes = os.listdir('resumes')
-for resume in resumes:
-    resumePaths.append(resumeStartPath + resume)
-
 def calculate_resume_similarity(text):
     resume = CountVectorizer(stop_words='english')
     count_matrix = resume.fit_transform(text)
-
     similarityPercentage = cosine_similarity(count_matrix)[0][1] * 100
-    similarityPercentage = round(similarityPercentage, 2)
+    similarityPercentage = round(similarityPercentage + 1, 2)
     return similarityPercentage
 
 def preprocess_text(text):
@@ -88,35 +54,95 @@ def preprocess_text(text):
     words = [word for word in words if word not in stop_words]
     return ' '.join(words)
 
-final_resume_data = {'Resume Name': [],
-                    'Keyword Hits': [],
-                    'Similarity Score': []
-}
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-selected_resumes = []
-for resumeFile in resumePaths:
-    file_extension = resumeFile.split('.')[-1].lower()
-    if file_extension == 'docx':
-        resume_text = extract_text_docx(resumeFile)
-    elif file_extension == 'pdf':
-        resume_text = extract_text_pdf(resumeFile)
-    elif file_extension == 'doc':
-        resume_text = extract_text_from_doc(resumeFile)
-    elif file_extension == 'rtf':
-        resume_text = extract_text_from_rtf(resumeFile)
-    elif file_extension == 'txt':
-        resume_text = extract_text_from_txt(resumeFile)
-    else:
-        print(f"Unsupported file type: {resumeFile}")
-        continue
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    if request.method == 'POST':
+        # Save uploaded files
+        job_description = request.files['jobdescription']
+        resumes = request.files.getlist('resumes')
+        keywords = request.files['keywords']
 
-    hits = screen_resume(resume_text, keywordsString)
-    resume_text = preprocess_text(resume_text)
-    text = [resume_text, cleanDescription]
-    similarity = calculate_resume_similarity(text)
-    final_resume_data['Resume Name'].append(resumeFile)
-    final_resume_data['Keyword Hits'].append(hits)
-    final_resume_data['Similarity Score'].append(similarity)
+        keywords.filename = 'keywords.txt'
+        job_description.filename = 'jobdescription.txt'
+        
+        keywords.save(os.path.join('uploads', keywords.filename))
+        job_description.save(os.path.join('uploads', job_description.filename))
+        for resume in resumes:
+            resume.save(os.path.join('uploads', resume.filename))
+        return redirect(url_for('process_files'))
 
-resumeDataFrame = pd.DataFrame(final_resume_data)
-resumeDataFrame.to_csv('ResumeData.csv', index=False)
+@app.route('/delete_files')
+def delete_files():
+    upload_folder = 'uploads'
+    for filename in os.listdir(upload_folder):
+        file_path = os.path.join(upload_folder, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+    return redirect(url_for('index'))
+
+@app.route('/process')
+def process_files():
+    # Read keywords and job description
+    with open('uploads/keywords.txt', 'r', encoding='utf-8') as keywordFile:
+        keywordsString = keywordFile.read().split(',')
+    
+    with open('uploads/jobdescription.txt', 'r') as descriptionFile:
+        descriptionString = descriptionFile.read()
+        
+    stop = stopwords.words('english')
+    cleanDescription = descriptionString.lower()
+    cleanDescription = re.sub(r'[^\w\s]', '', cleanDescription)
+    cleanDescription = cleanDescription.strip()
+    cleanDescription = re.sub('[0-9]+', '', cleanDescription)
+    cleanDescription = word_tokenize(cleanDescription)
+    cleanDescription = [w for w in cleanDescription if not w in stop] 
+    cleanDescription = ' '.join(cleanDescription)
+
+    resumePaths = []
+    resumeStartPath = "uploads/"
+
+    resumes = os.listdir('uploads')
+    for resume in resumes:
+        if ((resume != 'jobdescription.txt') and (resume != 'keywords.txt')):
+            resumePaths.append(resumeStartPath + resume)
+
+    final_resume_data = {'Resume Name': [], 'Keyword Hits': [], 'Similarity Score': []}
+    
+    for resumeFile in resumePaths:
+        file_extension = resumeFile.split('.')[-1].lower()
+        if file_extension == 'docx':
+            resume_text = extract_text_docx(resumeFile)
+        elif file_extension == 'pdf':
+            resume_text = extract_text_pdf(resumeFile)
+        elif file_extension == 'doc':
+            resume_text = extract_text_docx(resumeFile)
+        elif file_extension == 'rtf':
+            resume_text = extract_text_from_rtf(resumeFile)
+        elif file_extension == 'txt':
+            resume_text = extract_text_from_txt(resumeFile)
+        else:
+            continue
+
+        resume_text = preprocess_text(resume_text)
+        text = [resume_text, cleanDescription]
+
+        hits = screen_resume(resume_text, keywordsString)
+        similarity = calculate_resume_similarity(text)
+
+        totalscore = ((0.20 * similarity) + (0.80 * (hits)))
+        totalscore = round(totalscore, 2)
+
+        final_resume_data['Resume Name'].append(resumeFile.removeprefix("uploads/"))
+        final_resume_data['Keyword Hits'].append(hits)
+        final_resume_data['Similarity Score'].append(similarity)
+
+    resumeDataFrame = pd.DataFrame(final_resume_data)
+    resumeDataFrame.to_csv('static/ResumeData.csv', index=False)
+    return render_template('results.html', resume_data=resumeDataFrame.to_dict(orient='records'))
+
+if __name__ == '__main__':
+    app.run(debug=False)
